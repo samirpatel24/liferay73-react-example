aa3f6ac7fcb41b5e5493c2c747cf2a6f88661953
Samir Patel <samir.patel@liferay.com>
2020-04-11 00:52:18 +0530

    react example of resort

---
+++ src/pages/Rooms.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Hero from "../components/Hero";
+import { Link } from "react-router-dom";
+import Banner from "../components/Banner";
+import RoomsContainer from "../components/RoomsContainer";
+const Rooms = () => {
+  return (
+    <>
+      <Hero hero="roomsHero">
+        <Banner title="our rooms">
+          <Link to="/" className="btn-primary" />
+        </Banner>
+      </Hero>
+      <RoomsContainer></RoomsContainer>
+    </>
+  );
+};
+
+export default Rooms;
